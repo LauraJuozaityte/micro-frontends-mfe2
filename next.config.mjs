81dbb058c7537f2d1d20a3f704c43168306f3c59
lauraj <laura.juozaityte@accenture.com>
2024-09-25 12:06:58 +0300

    Working, but with warnings.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,83 @@
-/** @type {import('next').NextConfig} */
+import { NextFederationPlugin } from '@module-federation/nextjs-mf';
+
+/**
+ * @type {import('next').NextConfig}
+ */
+export default {
+  webpack: (config, options) => {
+    const { isServer } = options;
+    
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'mfe2',
+        filename: 'static/chunks/remoteEntry.js',
+
+        exposes: {
+          './Nav': './components/Nav.tsx', 
+          './add': './utils/add.ts',
+          './multiplyByTwo': './utils/multiplyByTwo.ts', 
+        },
+
+        shared: {
+          // shared modules
+        },
+      })
+    );
+    return config;
+  },
+};
+
+
+
+
+
+/*import { NextFederationPlugin } from '@module-federation/nextjs-mf';
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, options) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'mfe2',
+        filename: 'static/chunks/remoteEntry.js',
+
+        exposes: {
+          './Nav': './components/Nav.tsx', 
+          './add': './utils/add.ts',
+          './multiplyByTwo': './utils/multiplyByTwo.ts', 
+        },
+
+        remotes: {},
+
+        shared: {
+          react: {
+            singleton: true,
+            requiredVersion: '^18.0.0',  
+            eager: true,
+          },
+          'react-dom': {
+            singleton: true,
+            requiredVersion: '^18.0.0',  
+            eager: true,
+          },
+        },
+
+        initOptions: {
+          globalName: 'mfe2',
+          shareStrategy: 'loaded-first',
+        },
+
+        extraOptions: {
+          exposePages: true,
+          enableImageLoaderFix: true,
+          enableUrlLoaderFix: true,
+        },
+      })
+    )
+    return config
+  }
 };
 
 export default nextConfig;
+
+*/
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,49 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
+import {
+  revalidate,
+  FlushedChunks,
+  flushChunks,
+} from '@module-federation/nextjs-mf/utils';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+interface MyDocumentProps extends DocumentInitialProps {
+  chunks?: string[];
 }
+
+class MyDocument extends Document<MyDocumentProps> {
+  static async getInitialProps(ctx: DocumentContext): Promise<MyDocumentProps> {
+    if (ctx.pathname) {
+      if (!ctx.pathname.endsWith('_error')) {
+        await revalidate().then((shouldUpdate) => {
+          if (shouldUpdate) {
+            console.log('should HMR', shouldUpdate);
+          }
+        });
+      }
+    }
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    const chunks = (await flushChunks()) as string[];
+
+    return {
+      ...initialProps,
+      chunks,
+    };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          {this.props.chunks && <FlushedChunks chunks={this.props.chunks} />}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
